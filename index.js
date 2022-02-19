9bff77d7847cae8c20138b6c2f8a7aa2674db47e
zhaoyuanmeng <1426593075@qq.com>
2022-02-19 22:10:19 +0800

    feat:chalk

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 #!/usr/bin/env node
-
 const pckJosn = require('./package.json')
 const welcome = require('cli-welcome');
-
-// Use it.
-welcome({title: `Welcome CLI`, tagLine: `by Ahmad Awais`});
-
-// OR with all the options set like this:
+// chalk 添加颜色的
+const chalk = require('chalk');
+// import chalk from 'chalk';
+const log = console.log;
+const dim = chalk.dim;
+const italic = chalk.italic;
+const github = chalk.hex(`#1da1f2`).bold.inverse;
+const blog = chalk.hex('#6cc644').bold.inverse;
+// 欢迎项
 welcome({
-    title: `${pckJosn.name}`,
-    description:`${pckJosn.description}`,
+	title: `${pckJosn.name}`,
+	description: `${pckJosn.description}`,
 	tagLine: `Get to know zyd-cli`,
 	bgColor: `#FADC00`,
 	color: `#000000`,
@@ -17,10 +20,18 @@ welcome({
 	clear: true,
 	version: `${pckJosn.version}`
 });
-console.log(`
+log(
+`
+${italic(`
 zyd(赵元达)
-河北科技大学计算机学生
-Github: https://github.com/zhaoyuanmeng
-Blog: https://zhaoyuanmeng.github.io/
-(⊙o⊙)…
-`)
+河北科技大学计算机学生这里是我的简历河北科技大学计算机学生
+这里是我的简历河北科技大学计算机学生这里是我的简历河北科技
+大学计算机学生这里是我的简历`
+)}
+
+${github(' Github ')} ${dim('https://github.com/zhaoyuanmeng')}
+
+${blog(' Blog ')} ${dim('https://zhaoyuanmeng.github.io/')}
+
+`
+);
