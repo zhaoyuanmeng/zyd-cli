c92a5458afa818984f4365f7ac95531c08f33fa7
zhaoyuanmeng <1426593075@qq.com>
2022-02-19 20:32:32 +0800

    feat: add readme

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
+
+const pckJosn = require('./package.json')
+const welcome = require('cli-welcome');
+
+// Use it.
+welcome({title: `Welcome CLI`, tagLine: `by Ahmad Awais`});
+
+// OR with all the options set like this:
+welcome({
+    title: `${pckJosn.name}`,
+    description:`${pckJosn.description}`,
+	tagLine: `Get to know zyd-cli`,
+	bgColor: `#FADC00`,
+	color: `#000000`,
+	bold: true,
+	clear: true,
+	version: `${pckJosn.version}`
+});
 console.log(`
 zyd(赵元达)
 河北科技大学计算机学生
---
+++ readme.md
@@ -0,0 +1,10 @@
+# `npx zyd`
+
+>get to know zyd-cli using your command line with the Node.js
+
+## Usage
+To use this CLI run the following command:
+
+```sh
+npx zyd
+```
